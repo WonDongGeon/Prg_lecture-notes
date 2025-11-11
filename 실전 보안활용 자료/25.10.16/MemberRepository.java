a11d3efe23cb02a74fcd3e34b4dde02d769dfa1c
원동건 <xpdlf5103@naver.com>
2025-11-11 18:03:34 +0900

    Delete 실전 보안활용 자료 directory

--- 실전 보안활용 자료/25.10.16/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo3.entity.MemberEntity;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, Long>{
-    
-}
